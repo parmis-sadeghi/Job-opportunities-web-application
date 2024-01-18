cb4a2c4400a25f17e244eaae5829c34a7640b2de
parmis-sadeghi <89180717+parmis-sadeghi@users.noreply.github.com>
2024-01-18 16:50:34 +0330

    design-of-backend

---
+++ Job_Recommendation/Job_Recommendation/Form1.cs
@@ -0,0 +1,16 @@
+namespace Job_Recommendation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 Karjosignin = new Form2();
+            Karjosignin.Show();
+        }
+    }
+}
